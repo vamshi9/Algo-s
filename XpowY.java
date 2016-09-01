73c9ebf093770c18a231da1554bcd6e275d8f7c6
Vamshi Bachaneboina <vamsisai999@gmail.com>
2016-09-01 11:24:34 +0530

    Check if a number can be in form of x power y

---
+++ XpowY.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class XpowY{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    for( ;t>0;t--){
+      int num = sc.nextInt();
+      XpowY obj =  new XpowY();
+      boolean value = obj.findValue(num);
+      System.out.println(value);
+    }
+  }
+  public static boolean findValue(int num){
+    int x=0;
+    for(x=2;x<=Math.sqrt(num);x++){
+      while(num%x == 0){
+        num = num/x;
+        if(num==1){
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
